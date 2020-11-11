ea66758d818848958a9b325c6194ef4eda7bcdb2
Jérémy L'Hour <jlhourENSAE@users.noreply.github.com>
2020-11-11 13:16:25 +0100

    Functions to compute phi and zeta see Issue #1
    Not sure it's exact, needs to be checked

---
+++ dev_std.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 11 12:18:06 2020
+
+@author: jeremylhour
+"""
+
+import sys
+sys.path.append('/Users/jeremylhour/Documents/code/CIC-asymptotics/functions/')
+
+import numpy as np
+from scipy.stats import expon
+
+from func_ecdf import *
+from func_kde import *
+from func_main import *
+from func_simu import *
+
+
+# simulate data
+y, z, x, theta0 =  generate_data(distrib_y = expon(scale=10),
+                                 distrib_z = expon(scale=1),
+                                 distrib_x = expon(scale=.2),
+                                 size = 100)
+
+
+def compute_zeta(y, x, z, method="smoothed"):
+    """
+    compute_zeta:
+        compute vector zeta_i as in out paper,
+        similar to Q in Athey and Imbens (2006).
+    """
+    u_hat = counterfactual_ranks(points_to_predict=x, points_for_distribution=z, method=method)
+    denominateur = kernel_density_estimator(x=np.quantile(y, u_hat), data=y) 
+    
+    support = np.linspace(1/len(y), 1, len(y), endpoint=True) # = F_y(Y)
+    zeta = []
+    for point in support:
+        indicator = np.zeros(len(u_hat))
+        indicator[np.where(point <= u_hat)] = 1
+        inside_integral = -(indicator - u_hat)/denominateur
+        zeta.append(inside_integral.mean())
+        
+    return np.array(zeta)
+    
+
+def compute_phi(y, x, z, method="smoothed"):
+    """
+    compute_phi:
+        compute vector phi_i as in out paper,
+        similar to P in Athey and Imbens (2006).
+    """
+    u_hat = counterfactual_ranks(points_to_predict=x, points_for_distribution=z, method=method)
+    denominateur = kernel_density_estimator(x=np.quantile(y, u_hat), data=y)
+    
+    phi = []
+    for point in z:
+        indicator = np.zeros(len(u_hat))
+        indicator[np.where(point <= u_hat)] = 1
+        inside_integral = indicator-u_hat/denominateur
+        phi.append(inside_integral.mean())
+    
+    return np.array(phi)
+        
