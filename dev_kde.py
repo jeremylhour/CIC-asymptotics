ff39cf55c8182356cd11ab5122f9b6fea1ecd3e8
Jérémy L'Hour <jlhourENSAE@users.noreply.github.com>
2020-11-11 11:58:52 +0100

    Kernel density estimator finalized
    the kernel can be changed without problem, everything is vectorized using np broadcasting

--- dev_kde.py
+++ dev_kde.py
@@ -11,17 +11,21 @@ sys.path.append('/Users/jeremylhour/Documents/code/CIC-asymptotics/')
 
 import numpy as np
 from func_sd import *
+from scipy.stats import norm
+import matplotlib.pyplot as plt
 
-n = 100
+n = 1000
 data = norm.ppf(np.random.uniform(size=n))
 
 
 support = np.linspace(-5, 5, 1000, endpoint=False)
 
-y_1 = [kernel_density_estimator(point, data) for point in support]
+y_1 = kernel_density_estimator(support, data)
+y_2 = norm.pdf(support)
 
 fig, ax = plt.subplots(1, 1)
 ax.plot(support, y_1, '-k', lw=2, label='Empirical Density')
+ax.plot(support, y_2, '-r', lw=2, label='True Density')
 ax.set_xlabel('Support')
 ax.set_ylabel('Cumulative probability')
 ax.legend(fancybox=True, loc='right')
--- func_sd.py
+++ func_sd.py
@@ -9,15 +9,28 @@ Created on Tue Nov 10 18:26:34 2020
 import numpy as np
 
 def epanechnikov_kernel(x):
-    if abs(x) > np.sqrt(5):
-        y = 0
-    else:
-        y = (1-x**2/5) * 3/(4*np.sqrt(5))
+    """
+    epanechnikov_kernel:
+        Epanechnikov kernel function,
+        as suggested in Athey and Imbens (2006).
+        
+    :param x: np.array
+    """
+    y = (1-(x**2)/5) * 3/(4*np.sqrt(5))
+    y[np.where(abs(x) > np.sqrt(5))] = 0
     return y
 
-def kernel_density_estimator(x, data):
 
-    h = 1.06 * data.std() / (len(data)**(1/5))
+def kernel_density_estimator(x, data):
+    """
+    kernel_density_estimator:
+        implement kernel density estimator with Silverman's rule of thumb.
     
-    y = [epanechnikov_kernel((i-x)/h) for i in data]/h
-    return y.mean()
+    :param x: new points
+    :param data: data to estimate the function
+        
+    """
+    h_silverman = 1.06 * data.std() / (len(data)**(1/5))
+    y = (x[np.newaxis].T - data)/h_silverman # Broadcast to an array dimension len(x) * len(data)
+    y = epanechnikov_kernel(y)/h_silverman
+    return y.mean(axis=1)
