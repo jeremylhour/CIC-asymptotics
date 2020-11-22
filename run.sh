7221eb61679c513ac46657d074b28e6c42ecd0b8
Jérémy L'Hour <jlhourENSAE@users.noreply.github.com>
2020-11-22 11:57:05 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ python3 simulation.py config_simulation.yml
 
 # parallel run
 cd Documents/code/CIC-Asymptotics/
-Python3 create_uml_files.py # to create yml files
+python3 create_yml_files.py # to create yml files
 parallel -a files_list.txt python3 simulation.py
+python3 generate_latex.py
