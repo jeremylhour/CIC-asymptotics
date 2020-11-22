25dc364e8316ead2b9388d6d4717a499905017d8
Jérémy L'Hour <jlhourENSAE@users.noreply.github.com>
2020-11-22 11:40:33 +0100

    Closing issue #9 :+1:
    the resulting latex table is not pretty, but it works

--- generate_latex.py
+++ generate_latex.py
@@ -18,70 +18,74 @@ config_file = os.path.join(os.getcwd(),'main_config.yml')
 with open(config_file, 'r') as stream:
     config = yaml.safe_load(stream)
     
-nb_simu = config['nb_simu']
-sample_size = config['sample_size']
+B = config['nb_simu']
+
 lambda_x = config['lambda_x']
 lambda_z = config['lambda_z']
 alpha_y = config['alpha_y']
 
 
+########## CREATING TABLE ##########
+digits = 3
+models = ['standard','smoothed']
+metrics_set = ['bias', 'MAE', 'RMSE', 'Coverage rate', 'Quantile .95']
 
+counter = 0
 
-pickle_file = 'output/raw/simulations_B='+str(B)+'_lambda_x='+str(lambda_x)+'_lambda_z='+str(lambda_z)+'_alpha_y='+str(alpha_y)
+f = open('output/simulation_table.tex', "w")
+f.write('\n')
+f.write(r'\begin{table}')
+f.write('\n')
 
-favorite_color = pickle.load(open(pickle_file+'.p','rb') )
+for i in lambda_x:
+    for j in lambda_z:
+        for k in alpha_y:
+            pickle_file = 'output/raw/simulations_B='+str(B)+'_lambda_x='+str(i)+'_lambda_z='+str(j)+'_alpha_y='+str(k)
+            result = pickle.load(open(pickle_file+'.p','rb'))
+            param_line = r' & \multicolumn{'+str(len(metrics_set)*len(result))+'}{c}{'+'$\lambda_X$='+str(i)+', $\lambda_Z$='+str(j)+r', $\alpha_Y$='+str(k)+'}'  
+            param_line = param_line+'\\\\'
+            for model in models:
+                counter += 1
+                string = model
+                item = 'model'
+                sample_line = ' '
+                header = r'\begin{tabular}{l|'
+                for sample_size in result:
+                    sample_line = sample_line+ r' & \multicolumn{'+str(len(metrics_set))+'}{c}{'+'n='+str(sample_size)+'}'
+                    header = header + ('c'*len(metrics_set))
+                    for metric in metrics_set:
+                        string = string+' & '+str(round(result[sample_size][metric][model], digits))
+                        item = item+' & '+metric
+                string = string +'\\\\'
+                item = item +'\\\\'
+                sample_line = sample_line +'\\\\'
+                header = header + '}'
+                
+                ### WRITING
+                if counter == 1:
+                    f.write(header)
+                    f.write('\n')
+                    f.write(r'\toprule')
+                    f.write('\n')
+                    f.write(sample_line)
+                    f.write('\n')
+                    f.write(item)
+                    f.write('\n')
+                    f.write(r'\hline')
+                    f.write('\n')
+                if model =='standard':
+                    f.write(r'\hline')
+                    f.write('\n')
+                    f.write(param_line)
+                    f.write('\n')
 
-def latex_table(results, file, models=['standard','smoothed'], digits=3):
-    """
-    latex_table:
-        outputs a latex table from a list of results
-    :param results: list of results based on the format results[sample_size][metric][model]
-    :param file: name of the output file
-    """
-    metrics_set = ['bias', 'MAE', 'RMSE', 'Coverage rate', 'Quantile .95']
+                f.write(string)
+                f.write('\n')
 
-    k=0
-
-    f = open(file+'.tex', "a")
-    f.write('\n')
-    f.write(r'\begin{table}')
-    f.write('\n')
-
-    for model in models:
-        k += 1
-        string = model
-        item = 'model'
-        sample_line = ' '
-        header = r'\begin{tabular}{l|'
-        for sample_size in results:
-            sample_line = sample_line+ r' & \multicolumn{'+str(len(metrics_set))+'}{c}{'+str(sample_size)+'}'
-            header = header + ('c'*len(metrics_set))
-            for metric in metrics_set:
-                string = string+' & '+str(round(results[sample_size][metric][model], digits))
-                item = item+' & '+metric
-        string = string +'\\\\'
-        item = item +'\\\\'
-        sample_line = sample_line +'\\\\'
-        header = header + '}'
-        ### WRITING
-        if k == 1:
-            f.write(header)
-            f.write('\n')
-            f.write(r'\toprule')
-            f.write('\n')
-            f.write(sample_line)
-            f.write('\n')
-            f.write(item)
-            f.write('\n')
-            f.write(r'\hline')
-            f.write('\n')
-        f.write(string)
-        f.write('\n')
-    
-    f.write(r'\bottomrule')
-    f.write('\n')
-    f.write(r'\end{tabular}')
-    f.write('\n')
-    f.write(r'\end{table}')
-    f.write('\n')
-    f.close()
+f.write(r'\bottomrule')
+f.write('\n')
+f.write(r'\end{tabular}')
+f.write('\n')
+f.write(r'\end{table}')
+f.write('\n')
+f.close()
