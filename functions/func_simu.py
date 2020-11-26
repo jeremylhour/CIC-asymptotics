fixing a lot of issues, using analytical formula for theta_0

functions/func_simu.py
@@ -35,6 +35,20 @@ def true_theta(distrib_y, distrib_z, distrib_x, size = 10000):
     return theta
 
 
+def analytical_theta(alpha_y, lambda_z, lambda_x):
+    """
+    analytical_theta:
+        compute the true value of theta,
+        using an analytical formula.
+    
+    :param alpha_y:
+    :param lambda_z:
+    :param lambda_x:
+    """
+    theta = 1/(alpha_y*lambda_x/lambda_z - 1)
+    return theta
+
+
 def generate_data(distrib_y, distrib_z, distrib_x, size = 1000):
     """
     generate_data:
@@ -49,9 +63,9 @@ def generate_data(distrib_y, distrib_z, distrib_x, size = 1000):
     y = distrib_y.ppf(np.random.uniform(size=size))  
     z = distrib_z.ppf(np.random.uniform(size=size)) 
     x = distrib_x.ppf(np.random.uniform(size=size))   
-    theta0 = true_theta(distrib_y=distrib_y, distrib_z=distrib_z, distrib_x=distrib_x, size = 100000)
+    #theta0 = true_theta(distrib_y=distrib_y, distrib_z=distrib_z, distrib_x=distrib_x, size = 100000)
     
-    return y, z, x, theta0
+    return y, z, x
 
 
 ########## OBSERVED RANKS ##########

simulation.py
@@ -83,7 +83,7 @@ for sample_size in sample_size_set:
 
     random.seed(999)
 
-    results = np.zeros(shape=(B, 3))
+    results = np.zeros(shape=(B, 2))
     sigma = np.zeros(shape=(B, 2))
 
     start_time = time.time()
@@ -92,7 +92,7 @@ for sample_size in sample_size_set:
         sys.stdout.write("\r{0}".format(b))
         sys.stdout.flush()
         
-        y, z, x, theta0 =  generate_data(distrib_y = pareto(b=alpha_y, loc=-1),
+        y, z, x =  generate_data(distrib_y = pareto(b=alpha_y, loc=-1),
                                          distrib_z = expon(scale=1/lambda_z),
                                          distrib_x = expon(scale=1/lambda_x),
                                          size = sample_size)
@@ -101,13 +101,13 @@ for sample_size in sample_size_set:
         theta_standard, sigma_standard = estimator_unknown_ranks(y, x, z, method="standard")
         
         # Collecting results
-        results[b,] = [theta0, theta_smooth, theta_standard]
+        results[b,] = [theta_smooth, theta_standard]
         sigma[b,] = [sigma_smooth, sigma_standard]
     
         # Checking division error
         if math.isinf(sigma_smooth):
             print(' -- error for this iteration')
-            results[b,] = [np.nan]*3
+            results[b,] = [np.nan]*2
             sigma[b,] = [np.nan]*2
 
     print(f"Temps d'exécution total : {(time.time() - start_time):.2f} secondes ---")
@@ -119,10 +119,10 @@ for sample_size in sample_size_set:
     sigma = pd.DataFrame(sigma)
     sigma.dropna(axis=0, inplace=True)
     
-    theta0 = results[0].mean()
+    theta0 = analytical_theta(alpha_y = alpha_y, lambda_z = lambda_z, lambda_x = lambda_x)
     
-    y_hat = pd.DataFrame({'smoothed': results[1],
-                          'standard': results[2]})
+    y_hat = pd.DataFrame({'smoothed': results[0],
+                          'standard': results[1]})
     
     sigma_df = pd.DataFrame({'smoothed': sigma[0],
                              'standard': sigma[1]})

test_scripts/dev_analytical.py
@@ -19,12 +19,8 @@ from scipy.stats import expon, pareto
 
 
 
-def analytical_theta(lambda_x, alpha_y):
-    return 1/(alpha_y*lambda_x - 1)
-
-
 ########## PARAM ##########
-lambda_z = 1
+lambda_z = 1.5
 lambda_x = .9
 alpha_y = 3
 
@@ -33,20 +29,24 @@ print('Parameter values give b_2={:.2f}'.format(1-lambda_x/lambda_z))
 print('Parameter values give d_2={:.2f}'.format(1/alpha_y))
 print('So b_2+d_2={:.2f}'.format(1-lambda_x/lambda_z+1/alpha_y))
 
+if lambda_z > alpha_y*lambda_x:
+    print('Analytical formula is not working in that case.')
+
 ########## SIMULATED ##########
 theta_sim = true_theta(distrib_y = pareto(b=alpha_y, loc=-1),
                        distrib_z = expon(scale=1/lambda_z),
                        distrib_x = expon(scale=1/lambda_x),
-                       size = 100000)
+                       size = 1000000)
 
-print(theta_sim)
+print('Theta, value computed by Monte Carlo: {:.3f}'.format(theta_sim))
 
 ########## ANALYTICAL ##########
 
-theta_an = analytical_theta(lambda_x = lambda_x,
-                            alpha_y = alpha_y)
+theta_an = analytical_theta(alpha_y = alpha_y,
+                            lambda_z = lambda_z,
+                            lambda_x = lambda_x)
 
-print(theta_an)
+print('Theta, value computed analytically: {:.3f}'.format(theta_an))
 
 
 