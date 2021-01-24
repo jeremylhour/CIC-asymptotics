9b58404d78be78dac4dd6fb95e1746c31d5eae84
Jérémy L'Hour <jlhourENSAE@users.noreply.github.com>
2021-01-24 18:11:15 +0100

    refactoring

---
+++ DGP_gaussian/GAUSSIAN_create_yml_files.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Create YML files for parallel computing,
+from a main YML file.
+
+Created on Tue Nov 17 17:13:30 2020
+
+@author: jeremylhour
+"""
+
+import sys, os
+import yaml
+
+if not os.path.exists('input_configs_GAUSSIAN'):
+    os.makedirs('input_configs_GAUSSIAN')
+
+liste = 'files_list_GAUSSIAN.txt'
+
+########## INPUT PARAMETERS ##########
+config_file = os.path.join(os.getcwd(),'DGP_gaussian/GAUSSIAN_config.yml')
+
+with open(config_file, 'r') as stream:
+    config = yaml.safe_load(stream)
+    
+nb_simu = config['nb_simu']
+sample_size = config['sample_size']
+mu_x = config['mu_x']
+variance_x = config['variance_x']
+
+print('Creating requested YAML files...')
+
+for i in mu_x:
+    for j in variance_x:
+        file_name = 'input_configs_GAUSSIAN/mu_x='+str(i)+'_variance_x='+str(j)+'.yml'
+        f = open(file_name, "w")
+        f.write('nb_simu: {}       # Nb. of simulations, should be a scalar and the same for all files.\n'.format(nb_simu))
+        f.write('sample_size: {}    # Sample size, should be an array\n'.format(sample_size))
+        f.write('mu_x: {}        # Mean of X\n'.format(i))
+        f.write('variance_x: {}         # Variance of X\n'.format(j))
+        f.close()
+    
+        g = open(liste,'a')
+        g.write(file_name+'\n')
+        g.close()
---
+++ test_scripts/dev_dgp.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 23 10:46:53 2021
+
+@author: jeremylhour
+"""
+
+import numpy as np
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+
+
+def density(x, delta = 0, sigma0=1, sigma1=1):
+    
+    # Compute the density
+    quantile = norm.ppf(x)
+    y = (sigma0/sigma1)*np.exp(-((sigma0+sigma1)*quantile + delta)*((sigma0-sigma1)*quantile + delta)/(2*sigma1**2))
+    
+    # Compute the bound
+    b = (sigma0/sigma1)*(2*(1-x))**(sigma0**2/sigma1**2-1)
+    
+    return y, b
+
+
+n = 10000
+x_eval = np.linspace(.98, 1, n, endpoint=False)
+
+sigma0 = .2
+sigma1 = 1
+
+y, z = density(x_eval, delta=-2, sigma0=sigma0, sigma1=sigma1)
+
+print('b2 = '+str(1-sigma0**2/sigma1**2))
+
+if any(y>z):
+    print('The function goes over the bound. Problem detected.')
+
+plt.plot(x_eval, y)
+plt.plot(x_eval, z)
+
+axes = plt.gca()
+axes.set_xlim([.99,1])
+axes.set_ylim([0,5000])
+plt.show()
