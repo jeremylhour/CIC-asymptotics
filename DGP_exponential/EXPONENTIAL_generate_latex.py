70beafbed81cbc51b5fb188488d4d98d5902a25b
zctxti <jeremy.lhour@insee.fr>
2022-01-02 21:32:54 +0000

    bootstrap in exponential DGP

--- DGP_exponential/EXPONENTIAL_generate_latex.py
+++ DGP_exponential/EXPONENTIAL_generate_latex.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
                         continue
 
                     param_line = r' & \multicolumn{'+str(len(metrics_set)*len(result))+'}{c}{'+'$\lambda_X$='+str(i)+', $\lambda_Z$='+str(j)+r', $\alpha_Y$='+str(k)+' -- $b_2+d_2$='+ str(round(1-i/j+1/k, 2))+'}'  
-                    param_line = param_line+'\\\\'
+                    param_line += '\\\\'
                     for model in models:
                         counter += 1
                         string = model.replace('_', ' ')
@@ -60,21 +60,21 @@ if __name__ == '__main__':
                         sample_line = ' '
                         header = r'\begin{longtable}{l|'
                         for sample_size in result:
-                            sample_line = sample_line+ r' & \multicolumn{'+str(len(metrics_set))+'}{c}{'+'n='+str(sample_size)+'}'
-                            header = header + ('c'*len(metrics_set))
+                            sample_line += r' & \multicolumn{'+str(len(metrics_set))+'}{c}{'+'n='+str(sample_size)+'}'
+                            header += ('c'*len(metrics_set))
                             for metric in metrics_set:
-                                string = string+' & '+str(round(result[sample_size][metric][model], digits))
-                                item = item+' & '+metric
-                        string = string +'\\\\'
-                        item = item +'\\\\'
-                        sample_line = sample_line +'\\\\'
-                        header = header + '}'
+                                string += ' & '+str(round(result.get(sample_size).get(metric).get(model), digits))
+                                item += ' & '+metric
+                        string += '\\\\'
+                        item += '\\\\'
+                        sample_line += '\\\\'
+                        header += '}'
 
                         ### WRITING
                         if counter == 1:
                             f.write(header)
                             f.write('\n')
-                            f.write(r'\caption{}\\')
+                            f.write(r'\caption{}\\ \n')
                             f.write('\n')
                             f.write(r'\toprule')
                             f.write('\n')
@@ -91,9 +91,22 @@ if __name__ == '__main__':
                             f.write('\n')
 
                         if colours:
-                            f.write(r'\rowcolor{'+color_dico[i]+'} ')
+                            f.write(r'\rowcolor{'+color_dico.get(i)+'} ')
                         f.write(string)
                         f.write('\n')
+                        
+                    # Adding bootstrap
+                    string = "Bootstrap"
+                    for sample_size in result:
+                        for metric in metrics_set:
+                            if metric == 'Coverage rate':
+                                string += ' & '+str(round(result.get(sample_size).get('Bootstrap cov. rate'), digits))
+                            else:
+                                string += ' & '
+                    string += '\\\\'
+                    f.write(string)
+                    f.write('\n')
+                            
 
         f.write(r'\bottomrule')
         f.write('\n')
